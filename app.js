d5f09fedb162bd24713dc788ebb5a1a203a600af
Abdelali <El BAHLOUL>
2019-12-20 13:51:38 +0100

    adding namespace products and room

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const ioc = require('socket.io-client');
 
-const socket = ioc.connect("http://localhost:3000"); //connect the client socket to the socket server emited
+// const socket = ioc.connect("http://localhost:3000"); //connect the client socket to the socket server emited
 
-socket.on("welcome", (data) => {
+// socket.on("welcome", (data) => {
+//     console.log(`Receivied: `, data);
+    
+// })
+
+const products = ioc.connect("http://localhost:3000/products"); //connect namespace product emited
+
+products.on("product", (data) => {
     console.log(`Receivied: `, data);
     
-})
+});
 
+products.emit('joinRoom', 'Room1')
+        .on('error', err => {
+            console.log(err);
+            
+        })
+        .on('success', data => {
+            console.log(data);
+            
+        });
--- server.js
+++ server.js
@@ -4,11 +4,31 @@ const port = process.env.PORT || 3000;
 const server = http.createServer();
 const io = require('socket.io')(server);
 var nbr = 0;
-io.on('connection', (socket) => {
-    socket.emit("welcome", "Hello i'm a socket.io server");
-    nbr++;
-    console.log(`${nbr} client are connected`);
+// io.on('connection', (socket) => {
+//     socket.emit("welcome", "Hello i'm a socket.io server");
+//     nbr++;
+//     console.log(`${nbr} client are connected`);
     
+// });
+
+
+const productRoom = ["Room1", "Room2"];
+io
+    .of('/products') //add a namespace named products 
+    .on('connection', (socket) => {
+        socket.emit("product", "Hello i'm the namespace product");
+        nbr++;
+        console.log(`${nbr} client are connected`);
+
+        socket.on('joinRoom', room => {
+            if (productRoom.includes(room)) {
+                socket.join(room);
+                return socket.emit("success", "you are join the room"); 
+            } else {
+                return socket.emit('error', { msg: "404-not found! The room are u loking for is not exist" });
+            }
+        });
+        
 });
 
 server.listen(port, () => {
